4e62f2c8984df80223dbf06613060a0323fa3b5d
Shann Rainey <shannvrainey@gmail.com>
2018-05-23 21:18:50 -0700

    DontWorry

---
+++ Web Fundamentals/Algorithms 0 - 3/dontworry.js
@@ -0,0 +1,9 @@
+function beCheerful(){
+
+    var goodMorning = 0
+        while(goodMorning<100)
+        console.log("Good Morning!")
+        goodMorning = goodMorning + 1;
+
+    }
+    beCheerful();
