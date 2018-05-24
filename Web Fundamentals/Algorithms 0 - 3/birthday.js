5af31e33dd06601fe47c7c3f2021077d50a5fc69
Shann Rainey <shannvrainey@gmail.com>
2018-05-23 21:48:29 -0700

    Birthday

---
+++ Web Fundamentals/Algorithms 0 - 3/birthday.js
@@ -0,0 +1,17 @@
+function birthday(){
+
+    var birthday = "(4,19)"
+
+    if ( birthday == "(4,19)"){
+
+        console.log("How did you Know!");
+    }
+
+    else{
+        console.log ("Just another Day");
+    }
+
+
+}
+
+birthday ();
