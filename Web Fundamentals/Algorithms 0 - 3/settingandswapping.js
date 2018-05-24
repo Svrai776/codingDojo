9ae4d9c89c42909d3ff0a3b0cb8f36676f6d8f77
Shann Rainey <shannvrainey@gmail.com>
2018-05-23 20:57:43 -0700

    Setting & Swapping

---
+++ Web Fundamentals/Algorithms 0 - 3/settingandswapping.js
@@ -0,0 +1,14 @@
+function swap() {
+    var myNumber = 42;
+    var myName = 'Shann Rainey';
+    console.log('myNumber: ' + myNumber);
+    console.log('myName: ' + myName);
+
+    var temp = myNumber;
+    console.log("temp: " +  temp);
+
+    myNumber = myName;
+    myName = temp;
+
+}
+
