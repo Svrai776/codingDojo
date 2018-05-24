7fa348e5bdeacafb6ea7042a46d504a2f51f0051
Shann Rainey <shannvrainey@gmail.com>
2018-05-23 21:34:10 -0700

    Multiples  of 3

---
+++ Web Fundamentals/Algorithms 0 - 3/Multiples3.js
@@ -0,0 +1,21 @@
+function multiples(){
+
+    for ( var i = -300 ; i < 1 ; i++){
+
+        if ( i % 3 === 0){
+            i = i + 1;
+        }
+
+        if ( i == -3){
+            continue;
+        }
+
+        if ( i == -6){
+            continue;
+        }
+
+
+    }
+}
+
+multiples();
